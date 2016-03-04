f96b9fd749be079051042d91a18c64c9fb2f6081
Sunil Reddy <sunilpro@gmail.com>
2016-03-04 22:24:52 +0800

    Geo Testing node.js based server

---
+++ geo_testing_server/app.js
@@ -0,0 +1,23 @@
+var express = require('express'),
+        geo = require('./routes/geo');
+var morgan = require('morgan')
+var bodyParser = require('body-parser')
+
+var app = express();
+
+app.use(morgan('dev'));     /* 'default', 'short', 'tiny', 'dev' */
+//app.use(bodyParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+                                  extended: true
+                              }));
+
+app.get('/geo', geo.findAll);
+app.get('/geo/:id', geo.findById);
+app.post('/geo', geo.addGeo);
+app.put('/geo/:id', geo.updateGeo);
+app.delete('/geo/:id', geo.deleteGeo);
+
+app.listen(3000);
+console.log('Listening on port 3000...');
+
