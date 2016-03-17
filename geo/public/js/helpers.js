01f1276e011bc2eadb1276b4727ec9e11f641848
Sunil Reddy <sunilpro@gmail.com>
2016-03-17 12:53:16 +0800

    Added response length column

--- geo/public/js/helpers.js
+++ geo/public/js/helpers.js
@@ -6,10 +6,17 @@ exports.helpers['timeDiff'] = function(operand1, operand2) {
 
 exports.helpers['formatTS'] = function(ts) {
     var format = new Date(ts*1000);
-    return format;
+    return format.toISOString();
 }
 
 exports.helpers['trimString'] = function(passedString, startstring, endstring) {
    var theString = passedString.substring(0, 80);
    return theString
 };
+
+exports.helpers['formatBytes'] = function(bytes) {
+   if(bytes == 0) return '0 bytes';
+   var sizes = [' bytes', ' KB', ' MB', ' GB'];
+   var i = Math.floor(Math.log(bytes) / Math.log(1024));
+   return parseFloat((bytes / Math.pow(1024, i)).toFixed(2)) + sizes[i];
+}
