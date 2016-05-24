c3805fa5a58264ffa2def50b68d4fa39618b4767
Sunil Reddy <sunilpro@gmail.com>
2016-05-25 04:24:04 +0800

    Get detailed device info

--- farm/device_farm/lib/farm/controllers/farm.js
+++ farm/device_farm/lib/farm/controllers/farm.js
@@ -1,11 +1,14 @@
 var exports = module.exports;
 
-var IDeviceManager  = require('../models/idevicemanager');
+var IDeviceManager = require('../models/idevicemanager');
 
 var deviceManager = new IDeviceManager()
-exports.getDevices = function(req, res) {
-    deviceManager.listAll(function(err, res) {
-        console.log("-> getDevices", err, res)
+exports.getDevices = function (req, res) {
+    deviceManager.list(function (err, devices) {
+        console.log("-> getDevices", err, devices)
+        devices[0].getInfo(function (err, info) {
+            console.log("-> info", err, info)
+        })
+        res.status(200).send(devices);
     })
-    res.status(200).send([{}]);
 };
